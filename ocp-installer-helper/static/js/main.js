d56f377e78d2f2b2158be885019cbebbb004dc85
ailee-1111 <84674155+ailee-1111@users.noreply.github.com>
2025-07-18 15:12:40 +0900

    Update main.js

--- ocp-installer-helper/static/js/main.js
+++ ocp-installer-helper/static/js/main.js
@@ -1,1 +1,14 @@
-1
+function generateSshKey() {
+    const keyName = document.getElementById('key_name').value;
+    if (!keyName) { alert('SSH 키 이름을 입력하세요.'); return; }
+    
+    fetch('/generate-ssh-key', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ key_name: keyName })
+    })
+    .then(response => response.text())
+    .then(data => alert(data));
+}
+
+// loadClusterInfo, insertSshKey, loadMirrorSecret 등 나머지 JS 함수 구현 필요
