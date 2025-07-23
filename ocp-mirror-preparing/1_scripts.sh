1a8e1f0509895b2d6a0bcb19f749967ae891177d
ailee-1111 <84674155+ailee-1111@users.noreply.github.com>
2025-07-23 16:00:29 +0900

    Update 1_scripts.sh

--- ocp-mirror-preparing/1_scripts.sh
+++ ocp-mirror-preparing/1_scripts.sh
@@ -113,7 +113,7 @@ After=network.target
 User=$APP_USER
 Group=$APP_GROUP
 WorkingDirectory=$APP_TARGET_DIR
-ExecStart=$(command -v gunicorn) --workers 4 --bind 0.0.0.0:${APP_PORT} --timeout 300 app:app
+ExecStart=$(command -v gunicorn) --workers 4 --bind 0.0.0.0:${APP_PORT} --timeout 900 app:app
 Restart=always
 
 [Install]
